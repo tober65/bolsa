0e49a420097ffb3c0df737ee33aced1787859071
Myhyun <mattyihyun@gmail.com>
2020-08-16 11:28:38 -0700

    created sample portfolio column component

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,8 +1,7 @@
 import React from "react";
+import PortfolioColumns from "../components/Portfoliocolumns"
 import { Container } from "react-bootstrap";
 import { Row } from "react-bootstrap";
-import { Col } from "react-bootstrap";
-import { Card } from "react-bootstrap";
 import { Navbar } from "react-bootstrap";
 
 function Portfolio() {
@@ -12,44 +11,7 @@ function Portfolio() {
     </Navbar>
     <Container>
       <Row>
-        <Col sm={6} md={5} lg={4}>
-          <h2 className="mt-3 text-center">
-            Your Portfolio
-        </h2>
-          <h3 className="mt-3 text-center">
-            Total Value: $2,078.87
-        </h3>
-          <div className="mt-4 text-center">
-            <h5>
-              Portfolio Distribution
-            </h5>
-          //INSERT THE DISTRIBUTION GRAPH HERE
-          </div>
-        </Col>
-        <Col sm={6} md={7} lg={8}>
-          <Card className="mt-3">
-            <Container>
-              <Card.Body>
-                <Card.Title>
-                  <h5>
-                    Stocks
-                  </h5>
-                </Card.Title>
-                <Row>
-                  <Col lg={6}>
-                    USO - UNITED STATES OIL FUND
-                            </Col>
-                  <Col lg={3}>
-                    20 SHARES
-                            </Col>
-                  <Col lg={3}>
-                    $660.80
-                            </Col>
-                </Row>
-              </Card.Body>
-            </Container>
-          </Card>
-        </Col>
+        <PortfolioColumns></PortfolioColumns>
       </Row>
     </Container>
   </div>
