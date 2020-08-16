2ab8be320de86428a317744605e43332026cf3ca
lindyem <lindy.merwin@gmail.com>
2020-08-16 11:31:12 -0700

    mods

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import logo from "./logo.svg";
 import "./dashboard.css";
 import { useAuth } from "../../utils/auth";
 
@@ -12,16 +11,6 @@ function Dashboard() {
 
   return (
     <div className="Home">
-      <div className="Home-header">
-        <img src={logo} className="Home-logo" alt="logo" />
-        <h2>Welcome {user.email}</h2>
-      </div>
-      <p className="Home-intro">
-        <button onClick={goToEditProfile}>Go to Profile</button>
-        <button style={{ marginLeft: "1em" }} onClick={() => logout()}>
-          Logout
-        </button>
-      </p>
     </div>
   );
 }
