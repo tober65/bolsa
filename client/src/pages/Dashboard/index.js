7b70536e75d6a85b8ec9ae1646e7585208e5113e
lindyem <lindy.merwin@gmail.com>
2020-08-16 12:49:30 -0700

    Start searchbox

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import "./dashboard.css";
+import { Container } from 'react-bootstrap'
 import { useAuth } from "../../utils/auth";
+import SearchBox from "../../components/SearchBox";
+import "./dashboard.css";
 
 function Dashboard() {
-  const { user, logout } = useAuth();
-  const history = useHistory();
+  // const { user, logout } = useAuth();
+  // const history = useHistory();
 
-  const goToEditProfile = () => history.push("/portfolio");
+  //const goToEditProfile = () => history.push("/portfolio");
 
   return (
-    <div className="Home">
-    </div>
+    <Container>
+      <h1>Dashboard</h1>
+      <SearchBox />
+    </Container>
   );
 }
 
