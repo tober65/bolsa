074a8276cd6a268b20be3a76617ce7db7a696a59
lindyem <lindy.merwin@gmail.com>
2020-08-19 13:22:55 -0700

    Styles

--- client/src/components/CompanyNews/index.js
+++ client/src/components/CompanyNews/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card } from "react-bootstrap"
+import { Card, Row, Col } from 'react-bootstrap'
 import API from "../../utils/API";
 import moment from "moment";
 
@@ -15,35 +15,24 @@ function CompanyNews(props) {
   }, [props.selectedSymbol]);
 
   return (
-    <div className="card-body my-2">
-      <h4 className="card-title">{`${props.selectedSymbol.description} News`}</h4>
-      <div className="table-rep-plugin">
-        <div
-          className="table-responsive mb-0"
-          data-pattern="priority-columns"
-        >
-          <table id="companies" className="table table-striped">
-            <tbody>
-              {news.map((item) => (
-                <tr>
-                  <th>
-                    <span className="date">
-                      {moment(item.datetime * 1000).format("M/D")}
-                    </span>
-                  </th>
-                  <td>
-                    <a key={item.id} href={item.url} target="_blank">
-                      <div>{item.headline}</div>
-                    </a>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-);
+    <Card.Body className="my-2">
+      <Card.Title className="news-title">{`${props.selectedSymbol.description} News`}</Card.Title>
+      {news.map((item) => (
+        <Row className="news-row">
+          <Col xs={1}>
+            <span className="date">
+              {moment(item.datetime * 1000).format("M/D")}
+            </span>
+          </Col>
+          <Col xs={11}>
+            <a key={item.id} href={item.url} target="_blank">
+              <div>{item.headline}</div>
+            </a>
+          </Col>
+        </Row>
+      ))}
+    </Card.Body>
+  );
 }
 
 export default CompanyNews;
--- client/src/components/NewsColumn/index.js
+++ client/src/components/NewsColumn/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { Card, Row, Col } from 'react-bootstrap'
 import "./news.css";
 import API from "../../utils/API";
-import moment from "moment"
+import moment from "moment";
 
 function NewsColumn(props) {
   const [news, setNews] = useState([]);
@@ -15,36 +16,22 @@ function NewsColumn(props) {
   }, []);
 
   return (
-    <div id="news">
-      <div className="card-body my-2">
-        <h4 className="card-title">News</h4>
-        <div className="table-rep-plugin">
-          <div
-            className="table-responsive mb-0"
-            data-pattern="priority-columns"
-          >
-            <table id="companies" className="table table-striped">
-              <tbody>
-                {news.map((item) => (
-                  <tr>
-                    <th>
-                      <span className="date">
-                        {moment(item.datetime * 1000).format("M/D")}
-                      </span>
-                    </th>
-                    <td>
-                      <a key={item.id} href={item.url} target="_blank">
-                        <div>{item.headline}</div>
-                      </a>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Card.Body className="my-2">
+      <Card.Title className="news-title">News</Card.Title>
+      {news.map((item) => (
+        <Row className="news-row">
+          <Col xs={1}>
+            <span className="date">
+              {moment(item.datetime * 1000).format("M/D")}
+            </span></Col>
+          <Col xs={11}>
+            <a key={item.id} href={item.url} target="_blank">
+              <div>{item.headline}</div>
+            </a>
+          </Col>
+        </Row>
+      ))}
+    </Card.Body>
   );
 }
 
