2a6cc6769290a7c0105f8fe63e6f8712097ae1a2
lindyem <lindy.merwin@gmail.com>
2020-08-24 13:17:47 -0700

    styles Dash

--- client/src/components/StockPrice/index.js
+++ client/src/components/StockPrice/index.js
@@ -1,26 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
+import { Col, Row} from "react-bootstrap";
 import "./StockPrice.css";
 
 function StockPrice(props) {
   return (
-    <div className="stockprices p-1">
-      <Table>
-        <thead>
-          {props.selectedSymbol.displaySymbol} -{" "}
-          {props.selectedSymbol.description}
-        </thead>
-        <tbody className="prices">
-          <tr>
-            <td>Current Price</td>
-            <td>{props.price.c} US$</td>
-          </tr>
-          <tr>
-            <td>Previous Close</td>
-            <td>{props.price.pc} US$</td>
-          </tr>
-        </tbody>
-      </Table>
+    <div className="stockprices">
+      <Row className="selected-stock">
+        {props.selectedSymbol.displaySymbol} -{" "}
+        {props.selectedSymbol.description}
+      </Row>
+      <Row>
+        <Col>Current Price</Col>
+        <Col>{props.price.c} US$</Col>
+      </Row>
+
+      <Row>
+        <Col>Previous Close</Col>
+        <Col>{props.price.pc} US$</Col>
+      </Row>
     </div>
   );
 }
